64dff620783a3ab99f5c0e6fc22ff800c1a2986d
Xox67 <xoxoghm64@gmail.com>
2026-05-01 20:38:40 +0300

    Add files via upload

---
+++ golkolychat.py
@@ -0,0 +1,14 @@
+import socket
+import time
+import os
+import pyperclip
+import random
+from PIL import Image
+import datetime
+hak=5
+while hak>0:
+    ert=input("sifre:")
+    if ert == "3169":
+        print("jsjs")
+        break
+
